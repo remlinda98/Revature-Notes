75fa0dd1d2d1b4918829c0330fb01271e986bbc1
Remington Linda <RemingtonLinda@Remingtons-MacBook-Pro.local>
2021-08-23 14:31:55 -0500

    add HelloWorld class

---
+++ FirstJavaProject/src/com/revature/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+public class HelloWorld {
+
+	// This is a line comment
+	public static void main(String[] args) {
+		/**
+		 * This 
+		 * is
+		 * multi-line
+		 */
+		System.out.println("Hello World... From the IDE");
+		
+	}
+
+}
